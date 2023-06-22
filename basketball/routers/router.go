dccfe37c1676a77aea1ad7fbb28f154b634724b7
nntrifonova <n.trifonova@g.nsu.ru>
2023-06-22 20:07:47 +0700

    B-13. /basketball go-Admin set up

--- basketball/routers/router.go
+++ basketball/routers/router.go
@@ -3,10 +3,8 @@ package routers
 import (
 	"basketball/controllers"
 	"github.com/astaxie/beego"
-	"github.com/beego/admin"
 )
 
 func init() {
-	admin.Run()
 	beego.Router("/", &controllers.MainController{})
 }
